0f30d9828d9891df09b7026dedfeb06595cef8fc
Flier Lu <flier.lu@gmail.com>
2017-02-28 15:38:23 +0800

    install librdkafka++

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -6,7 +6,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install thrift librdkafka
 else
     curl -sSL "http://apache.mirrors.spacedump.net/thrift/$THRIFT_VERSION/thrift-$THRIFT_VERSION.tar.gz" -o thrift.tar.gz
-
     mkdir -p thrift-$THRIFT_VERSION
     tar zxf thrift.tar.gz -C thrift-$THRIFT_VERSION --strip-components=1
     rm thrift.tar.gz
@@ -16,4 +15,15 @@ else
     sudo make install
     cd ..
     rm -rf thrift-$THRIFT_VERSION
+
+    curl -sSL "https://github.com/edenhill/librdkafka/archive/v$RDKAFKA_VERSION.tar.gz" -o librdkafka.tar.gz
+    mkdir -p librdkafka-$RDKAFKA_VERSION
+    tar zxf librdkafka.tar.gz -C librdkafka-$RDKAFKA_VERSION --strip-components=1
+    rm librdkafka.tar.gz
+    cd librdkafka-$RDKAFKA_VERSION
+    ./configure
+    make
+    sudo make install
+    cd ..
+    rm -rf librdkafka-$RDKAFKA_VERSION
 fi

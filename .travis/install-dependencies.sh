fb6875f06037a2ca8d2ca69b0435220fbb0027af
Flier Lu <flier.lu@gmail.com>
2017-03-16 16:40:08 +0800

    install latest CMake

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -4,4 +4,7 @@ set -e
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew install curl double-conversion gflags glog google-benchmark gperftools rapidjson folly thrift librdkafka grpc doxygen
+else
+    wget --no-check-certificate https://cmake.org/files/v3.7/cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
+    tar -xvf cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
 fi

eed58757069ff7c44ca3a828a545501d2162f27e
Flier Lu <flier.lu@gmail.com>
2017-03-17 20:56:12 +0800

    check cmake bin file

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew install ccache curl double-conversion gflags glog google-benchmark gperftools rapidjson folly thrift librdkafka grpc doxygen
 else
-    if [[ ! -d "cmake-$CMAKE_VERSION-Linux-x86_64" ]]
+    if [[ ! -f "cmake-$CMAKE_VERSION-Linux-x86_64/bin/cmake" ]]
     then
         wget --no-check-certificate https://cmake.org/files/v3.7/cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
         tar -xvf cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz

c08e1972ae125fd1cbb82fae879bf1494f65c752
Flier Lu <flier.lu@gmail.com>
2017-03-17 19:22:37 +0800

    use cached cmake

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -5,6 +5,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew install ccache curl double-conversion gflags glog google-benchmark gperftools rapidjson folly thrift librdkafka grpc doxygen
 else
-    wget --no-check-certificate https://cmake.org/files/v3.7/cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
-    tar -xvf cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
+    if [[ ! -d "cmake-$CMAKE_VERSION-Linux-x86_64" ]]
+    then
+        wget --no-check-certificate https://cmake.org/files/v3.7/cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
+        tar -xvf cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
+    fi
 fi

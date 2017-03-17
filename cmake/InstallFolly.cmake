5f84e061cf0a0c52a94e52f8a2caa9f21c11c787
Flier Lu <flier.lu@gmail.com>
2017-03-17 22:39:00 +0800

    link libc++ when use clang

--- cmake/InstallFolly.cmake
+++ cmake/InstallFolly.cmake
@@ -16,9 +16,7 @@ if (NOT FOLLY_FOUND OR USE_BUNDLED_FOLLY)
                             PKG_CONFIG_PATH=<INSTALL_DIR>/lib/pkgconfig
                             CFLAGS=-I<INSTALL_DIR>/include
                             CXXFLAGS=-I<INSTALL_DIR>/include
-                        if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-                            CPPFLAGS=-stdlib=libc++
-                        endif ()
+                            CPPFLAGS=-stdlib=${STDLIB}
                             LIBS=${CMAKE_THREAD_LIBS_INIT}
                             OPENSSL_CFLAGS=-I${OPENSSL_INCLUDE_DIR}
                             OPENSSL_LIBS=-L${OPENSSL_LIBRARY_DIR}

daa2a621df3f30d00d4f68861212a9fc8bbeaa09
Flier Lu <flier.lu@gmail.com>
2017-06-29 17:35:08 +0800

    upgrade cmake to v3.8.2

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -7,7 +7,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 else
     if [[ ! -f "cmake-$CMAKE_VERSION-Linux-x86_64/bin/cmake" ]]
     then
-        wget --no-check-certificate https://cmake.org/files/v3.7/cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
+        wget --no-check-certificate https://cmake.org/files/v3.8/cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
         tar -xvf cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
     fi
 fi

8807f41b31cf63962a0d9e7522dee7a79041ae8a
Flier Lu <flier.lu@gmail.com>
2017-08-11 16:01:26 +0800

    remove old and run new container

--- ci/build-dist.sh
+++ ci/build-dist.sh
@@ -10,30 +10,6 @@ DIST_DIR=${BASE_DIR}/dist
 IMAGE_NAME=zipkin-cpp-build:ubuntu
 CONTAINER_NAME=zipkin-cpp-build
 
-function container_has_outdated {
-    local image_name=$1
-    local container_name=$2
-    local image_id=`docker ps -a -q -f "name=${container_name}" --format="{{.Image}}"`
-
-    [[ $image_id != "" && $image_id != $image_name ]]
-}
-
-function container_is_exists {
-    local image_name=$1
-    local container_name=$2
-    local container_id=`docker ps -a -q -f "name=${container_name}" -f "ancestor=${image_name}"`
-
-    [[ -n $container_id ]]
-}
-
-function container_has_stopped {
-    local image_name=$1
-    local container_name=$2
-    local container_id=`docker ps -q -f "name=${container_name}" -f "ancestor=${image_name}"`
-
-    [[ -z running_container_id ]]
-}
-
 # When running docker on a Mac, root user permissions are required.
 if [[ "$OSTYPE" == "darwin"* ]]; then
     USER=root
@@ -43,26 +19,34 @@ else
     USER_GROUP=$(id -g)
 fi
 
-if container_has_outdated ${IMAGE_NAME} ${CONTAINER_NAME}; then
-    echo "remove outdated container ${OLD_CONTAINER_ID}"
+function container_is_exists {
+    local CONTAINER_ID=`docker ps -a -q -f "name=${CONTAINER_NAME}" -f "ancestor=${IMAGE_NAME}"`
 
-    docker rm ${CONTAINER_NAME}
-fi
+    [[ -n $CONTAINER_ID ]]
+}
 
-if container_is_exists ${IMAGE_NAME} ${CONTAINER_NAME}; then
-    if container_has_stopped ${IMAGE_NAME} ${CONTAINER_NAME}; then
-        echo "start the container"
+function container_is_running {
+    local CONTAINER_ID=`docker ps -q -f "name=${CONTAINER_NAME}" -f "ancestor=${IMAGE_NAME}"`
 
-        docker start ${CONTAINER_NAME}
-    fi
+    [[ -n $CONTAINER_ID ]]
+}
 
-    echo "attach to the container"
-
-    docker logs -f ${CONTAINER_NAME}
-else
+function run_container {
     echo "run new container"
 
     docker run --name ${CONTAINER_NAME} -u "${USER}":"${USER_GROUP}" -v ${DIST_DIR}:/zipkin-cpp/dist ${IMAGE_NAME} $1
+}
+
+if container_is_exists; then
+    if container_is_running; then
+        docker logs -f ${CONTAINER_NAME}
+    else
+        docker rm ${CONTAINER_NAME}
+
+        run_container
+    fi
+else
+    run_container
 fi
 
 PKG_TYPE=$2

acf2d81317fa58d77ffbcbf5faa9be87bc870d7c
Flier Lu <flier.lu@gmail.com>
2017-08-11 15:48:26 +0800

    check container status

--- ci/build-dist.sh
+++ ci/build-dist.sh
@@ -10,6 +10,30 @@ DIST_DIR=${BASE_DIR}/dist
 IMAGE_NAME=zipkin-cpp-build:ubuntu
 CONTAINER_NAME=zipkin-cpp-build
 
+function container_has_outdated {
+    local image_name=$1
+    local container_name=$2
+    local image_id=`docker ps -a -q -f "name=${container_name}" --format="{{.Image}}"`
+
+    [[ $image_id != "" && $image_id != $image_name ]]
+}
+
+function container_is_exists {
+    local image_name=$1
+    local container_name=$2
+    local container_id=`docker ps -a -q -f "name=${container_name}" -f "ancestor=${image_name}"`
+
+    [[ -n $container_id ]]
+}
+
+function container_has_stopped {
+    local image_name=$1
+    local container_name=$2
+    local container_id=`docker ps -q -f "name=${container_name}" -f "ancestor=${image_name}"`
+
+    [[ -z running_container_id ]]
+}
+
 # When running docker on a Mac, root user permissions are required.
 if [[ "$OSTYPE" == "darwin"* ]]; then
     USER=root
@@ -19,16 +43,26 @@ else
     USER_GROUP=$(id -g)
 fi
 
-if [[ -z `docker ps -a -q -f "name=${CONTAINER_NAME}" -f "ancestor=${IMAGE_NAME}"` ]]; then
-    docker run --name ${CONTAINER_NAME} -u "${USER}":"${USER_GROUP}" -v ${DIST_DIR}:/zipkin-cpp/dist ${IMAGE_NAME} $1
-else
-    CONTAINER_ID=`docker ps -q -f "name=${CONTAINER_NAME})" -f "ancestor=${IMAGE_NAME}"`
+if container_has_outdated ${IMAGE_NAME} ${CONTAINER_NAME}; then
+    echo "remove outdated container ${OLD_CONTAINER_ID}"
 
-    if [[ -z ${CONTAINER_ID} ]]; then
-        docker start ${CONTAINER_ID}
+    docker rm ${CONTAINER_NAME}
+fi
+
+if container_is_exists ${IMAGE_NAME} ${CONTAINER_NAME}; then
+    if container_has_stopped ${IMAGE_NAME} ${CONTAINER_NAME}; then
+        echo "start the container"
+
+        docker start ${CONTAINER_NAME}
     fi
 
-    docker logs -f ${CONTAINER_ID}
+    echo "attach to the container"
+
+    docker logs -f ${CONTAINER_NAME}
+else
+    echo "run new container"
+
+    docker run --name ${CONTAINER_NAME} -u "${USER}":"${USER_GROUP}" -v ${DIST_DIR}:/zipkin-cpp/dist ${IMAGE_NAME} $1
 fi
 
 PKG_TYPE=$2
